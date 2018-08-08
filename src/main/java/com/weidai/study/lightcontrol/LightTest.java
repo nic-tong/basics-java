0ac3d123bd6aca4090b887729667924753f7bf1e
tonglianghong <tonglianghong@wdai.com>
2018-08-08 09:42:19 +0800

    添加解说

--- src/main/java/com/weidai/study/lightcontrol/LightTest.java
+++ src/main/java/com/weidai/study/lightcontrol/LightTest.java
@@ -4,7 +4,7 @@
 package com.weidai.study.lightcontrol;
 
 /**
- * @Description: TODO
+ * @Description: 用wait 和 notify  实现一个灯 反复开 关的 信号灯。 一个线程负责开灯， 一个线程负责关灯。很像kafka 的消费者生产者模型。
  * @ClassName: LightTest
  * @author nic
  * @date: 2018年8月8日 上午9:12:20
