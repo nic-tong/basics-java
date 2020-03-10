7dea9ab7168386d293250b7eaaf6b115e6c25fee
lianghong.tlh <lianghong.tlh@alibaba-inc.com>
2020-03-11 00:56:54 +0800

    roman2Integer

--- src/main/java/com/weidai/study/dynamic/programming/MaxLengthSubStringDP.java
+++ src/main/java/com/weidai/study/dynamic/programming/MaxLengthSubStringDP.java
@@ -8,4 +8,53 @@ package com.weidai.study.dynamic.programming;
  * @date 2020/03/09
  */
 public class MaxLengthSubStringDP {
+
+    public  static  String maxLengthSubString(String str1, String str2) {
+        if(str1.length() == 0 || str2.length() == 0) {
+            return "";
+        }
+
+        int [][] array = new int[str1.length()][str2.length()];
+        int maxI = 0;
+        int maxLength = 0;
+        for(int i=0; i< str1.length();i++) {
+            if(str1.charAt(i) == str2.charAt(0)) {
+                array[i][0]= 1;
+                maxI = i;
+                maxLength = 1;
+            } else {
+                array[i][0] = 0;
+            }
+        }
+        for(int j=1; j< str2.length(); j++) {
+            if(str1.charAt(0) == str2.charAt(j)) {
+                array[0][j]= 1;
+                if(maxLength < 1) {
+                    maxLength = 1;
+                }
+            } else {
+                array[0][j] = 0;
+            }
+        }
+
+        for(int i=1; i<str1.length();i++) {
+            for(int j=1; j< str2.length(); j++) {
+                if(str1.charAt(i) == str2.charAt(j)) {
+                    array[i][j] = array[i-1][j-1] + 1;
+                    if(maxLength < array[i][j]) {
+                        maxI = i;
+                        maxLength = array[i][j];
+                    }
+                } else {
+                    array[i][j] = 0;
+                }
+            }
+        }
+
+        return str1.substring(maxI-maxLength+1,maxI + 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxLengthSubString("ababc", "ab"));
+    }
 }
---
+++ src/main/java/com/weidai/study/leetcode/A14_LongestCommonPrefix.java
@@ -0,0 +1,52 @@
+package com.weidai.study.leetcode;
+
+import static com.weidai.study.dynamic.programming.MaxLengthSubStringDP.maxLengthSubString;
+
+/**
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ *
+ * If there is no common prefix, return an empty string "".
+ *
+ * Example 1:
+ *
+ * Input: ["flower","flow","flight"]
+ * Output: "fl"
+ *
+ * @author lianghong.tlh
+ * @date 2020/03/11
+ */
+public class A14_LongestCommonPrefix {
+
+    public static String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0){
+            return "";
+        }
+        String maxPrefix = strs[0];
+        for(int i=1; i< strs.length;i++) {
+            if("".equals(maxPrefix)) {
+                return "";
+            }
+            maxPrefix = maxPrefixSubString(maxPrefix, strs[i]);
+        }
+        return maxPrefix;
+    }
+
+    private static String maxPrefixSubString(String maxPrefix, String str) {
+        int minLength = maxPrefix.length() > str.length()? str.length(): maxPrefix.length();
+        int maxSameChar = 0;
+        for(int i=0; i< minLength; i++) {
+            if(maxPrefix.charAt(i) == str.charAt(i)){
+                maxSameChar++;
+            }else {
+                break;
+            }
+        }
+        return maxPrefix.substring(0, maxSameChar);
+    }
+
+    public static void main(String[] args) {
+        String []strs = {"dog","racecar","car"};
+
+        System.out.println("prefix:"+longestCommonPrefix(strs));
+    }
+}
